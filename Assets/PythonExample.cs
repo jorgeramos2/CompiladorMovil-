634d6818393cbde7a18d5804854d874e613027af
jorgeramos2 <jorge.ramos1996@gmail.com>
2020-09-24 19:13:03 -0500

    commit inicial

---
+++ Assets/PythonExample.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IronPython.Hosting;
+using UnityEngine;
+
+//Este script realiza la conexión con python y ejecuta el script Greeter.
+public class PythonExample : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        //Instrucciones para inizializar ironpython
+        var engine = Python.CreateEngine();
+        ICollection<string> searchPaths = engine.GetSearchPaths();
+
+        //Path to the folder of greeter.py
+        searchPaths.Add(Application.dataPath);
+        //Path to the Python standard library
+        searchPaths.Add(Application.dataPath + @"\Plugins\Lib\");
+        engine.SetSearchPaths(searchPaths);
+
+        dynamic py = engine.ExecuteFile(Application.dataPath + @"\greeter.py");
+        dynamic greeter = py.Greeter("Mika");
+
+        //Desplegar la ejecución del script
+        Debug.Log(greeter.greet());
+        Debug.Log(greeter.random_number(1,5));
+    }
+
+    //Repetir el código en otra función nadamas para probarlo dando click en un botón
+    public void Display()
+    {
+        //Instrucciones para inizializar ironpython
+        var engine = Python.CreateEngine();
+        ICollection<string> searchPaths = engine.GetSearchPaths();
+
+        //Path to the folder of greeter.py
+        searchPaths.Add(Application.dataPath);
+        //Path to the Python standard library
+        searchPaths.Add(Application.dataPath + @"\Plugins\Lib\");
+        engine.SetSearchPaths(searchPaths);
+
+        dynamic py = engine.ExecuteFile(Application.dataPath + @"\greeter.py");
+        dynamic greeter = py.Greeter("Mika");
+
+        //Desplegar la ejecución del script
+        Debug.Log(greeter.greet());
+        Debug.Log(greeter.random_number(1,5));
+    }
+    
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/WriteFile.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+
+//Este script sirve para obtener texto de un inputfield y escribirlo en un archivo. 
+public class WriteFile : MonoBehaviour
+{
+    //Obtiene el inputfield donde se escribirá el código
+    public InputField mainInputField;
+
+    //Función para escribir el texto obtenido del inputfield en un archivo.
+    public void Write()
+    {
+
+     
+        String ObjectsText = mainInputField.text;
+
+
+        //Path del archivo
+        string path = Application.dataPath + "MyTest.txt";
+
+        //Si no existe el archivo lo crea y escribe
+        if (!File.Exists(path))
+        {
+            //Crear archivo.
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                //Escribir.
+                sw.WriteLine(ObjectsText);
+            }
+        }
+
+        //Si el archivo ya existe, lo borra, crea de nuevo, y escribe.
+        else
+        {
+            //Borrar archivo anterior.
+            File.Delete(path);
+            //Crear archivo.
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                //Escribir.
+                sw.WriteLine(ObjectsText);
+            }
+        }
+
+        // Open the file to read from.
+        /*
+        using (StreamReader sr = File.OpenText(path))
+        {
+            string s;
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
