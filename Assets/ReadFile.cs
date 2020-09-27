76d043596d3477ad3b36eb82ee2559ac4a042f3e
Arturo Méndez <arturomendez97@gmail.com>
2020-09-26 23:42:21 -0500

    created readFile script to read a text file and display the contents on a text box

---
+++ Assets/ReadFile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+
+public class ReadFile : MonoBehaviour
+{
+
+    public Text textField;
+
+    public void Read()
+    {
+        //Path del archivo
+        string path = Application.dataPath + "MyTest.txt";
+
+        //Si no existe el archivo
+        if (!File.Exists(path))
+        {
+                //Error.
+                Debug.Log("El archivo no existe.");
+        }
+        //SI el archivo si existe, lo lee
+        else{
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                string text = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    //Debug.Log("yay");
+                    //Console.WriteLine(s);
+                    text += s;
+                }
+
+                textField.text = text;
+            }
+        }
+
+    }
+}
